36af1926ffd6d84e63371de34678555bf239326d
Colin Keating <ckeating3000@gmail.com>
2017-04-30 19:07:12 -0500

    building get_month_summary

--- get_month_summary.py
+++ get_month_summary.py
@@ -12,19 +12,20 @@ from os.path import isfile, join
 if len(sys.argv)!=3:
 	sys.exit ("Usage: Python get_month_summary.py [mm-yy] [master_spreadsheet_filename]")
 
-
 #parameters needed (per building):
 #Averages: 
 #	   	total unit, lights, kitchen, laundry, HVAC, outlets, microwave, water heating 
 #Unit: 
 #		total unit, lights, kitchen, laundry, HVAC, outlets, microwave, water heating 
 date = sys.argv[1]
+if len(date)!=5:
+	sys.exit ("Usage: Python get_month_summary.py [mm-yy] [master_spreadsheet_filename]")
 month = int(date.split("-")[0])
 year = int(date.split("-")[1])
 
 master_name = sys.argv[2]
-print "month: " + month
-print "year: " + year
+print "month: " + str(month)
+print "year: " + str(year)
 print "file name: " + master_name
 full_month = []
 
@@ -36,15 +37,26 @@ with open(master_name, 'rb') as myfile: # open master file for reading and writi
     for line in reader:
     	#get the month and year from that line
     	date_time = line[0]
-    	temp_month = date_time.split("/")[0]
+    	temp_month = int(date_time.split("/")[0])
     	temp_year = date_time.split("/")[2]
-    	temp_year = temp_year.split(" ")[0]
-    	print "temp month: " + temp_month
-    	print "temp_year: " + temp_year
+    	temp_year = int(temp_year.split(" ")[0])
+    	print "temp month: " + str(temp_month)
+    	print "temp_year : " + str(temp_year)
     	if (temp_year==year and temp_month==month):
     		full_month.append(line)
 
+# 
+Buildings  = {'745', '749'}
+Units      = {'1N', '1S' ,'2N','2S','3N','3S'}
+Subsystems = {'HVAC','Kitchen','Laundry','Washer','Dryer','Lights','GenRec','Microwave_N','Microwave_S','MakeupAir_N','MakeupAir_S','Water_Heater_N','Water_Heater_S','Spare'}
+
+total_745=0.0
+total_749=0.0
 for line in full_month:
-	print line
+	if(line[1]) == "745":
+		total_745 = total_745+float(line[3])
+
+print total_745
+print total_749
 
 print "DONE"
--- sql_csv_parse_5.py
+++ sql_csv_parse_5.py
@@ -82,14 +82,14 @@ for file in listdir("."):
                                'G3L3-3A1':'1S' , 'G3L3-3A2':'1S' , 'G3L3-3A3':'1N' , 'G3L3-3A4':'1N' , 'G3L3-3A5':'2S' , 'G3L3-3A6':'2S' , 'G3L3-3A7':'2N' , 'G3L3-3A8':'2N' ,
                                'G3L3-3B1':'3S' , 'G3L3-3B2':'3S' , 'G3L3-3B3':'3N' , 'G3L3-3B4':'3N' , 'G3L3-3B5':'Water_Heater_N' , 'G3L3-3B6':'Microwave_N' , 'G3L3-3B7':'Water_Heater_S' , 'G3L3-3B8':'Microwave_S'
                             }
-                            #*** I'm almost positive that G3L2-2A1 should be "3S" and not 2S as listed in raw data.  This fixes error where 2S has two "GenRec" monitors and gives 3S what is presumably its rightful GenRec monitor
+                            #*** Correction to raw schematic: I'm almost positive that G3L2-2A1 should be "3S" and not 2S as listed in raw data.  This fixes error where 2S has two "GenRec" monitors and gives 3S what is presumably its rightful GenRec monitor
         # create second dictionary between meter code and subsystem
         MeterID_to_Subsys ={    # 749
                                 'G2L1-1A1':'GenRec'  , 'G2L1-1A2':'Spare'  , 'G2L1-1A3':'Kitchen' , 'G2L1-1A4':'Lights' , 'G2L1-1A5':'GenRec'    , 'G2L1-1A6':'Lights'    , 'G2L1-1A7':'GenRec'    , 'G2L1-1A8':'Spare1'    , 
                                 'G2L1-1B1':'Kitchen' , 'G2L1-1B2':'Lights' , 'G2L1-1B3':'Kitchen' , 'G2L1-1B4':'Lights' , 'G2L1-1B5':'GenRec'    , 'G2L1-1B6':'Spare1'    , 'G2L1-1B7':'Kitchen'   , 'G2L1-1B8':'Spare2'    ,
-                                'G2L2-2A1':'Kitchen' , 'G2L2-2A2':'Lights' , 'G2L2-2A3':'GenRec'  , 'G2L2-2A4':'Spare2' , 'G2L2-2A5':'Spare'     , 'G2L2-2A6':'Kitchen'   , 'G2L2-2A7':'Lights'    , 'G2L2-2A8':'GenRec'    ,
+                                'G2L2-2A1':'Kitchen' , 'G2L2-2A2':'Lights' , 'G2L2-2A3':'GenRec'  , 'G2L2-2A4':'Spare' , 'G2L2-2A5':'Spare'     , 'G2L2-2A6':'Kitchen'   , 'G2L2-2A7':'Lights'    , 'G2L2-2A8':'GenRec'    ,
                                 'G2L3-3A1':'Laundry' , 'G2L3-3A2':'HVAC'   , 'G2L3-3A3':'Laundry' , 'G2L3-3A4':'HVAC'   , 'G2L3-3A5':'Laundry'   , 'G2L3-3A6':'HVAC'      , 'G2L3-3A7':'Laundry'   , 'G2L3-3A8':'HVAC'      ,
-                                'G2L3-3B1':'Laundry' , 'G2L3-3B2':'HVAC'   , 'G2L3-3B3':'Laundry' , 'G2L3-3B4':'HVAC'   , 'G2L3-3B5':'Microwave' , 'G2L3-3B6':'MakeupAir' , 'G2L3-3B7':'Microwave' , 'G2L3-3B8':'MakeupAir' ,
+                                'G2L3-3B1':'Laundry' , 'G2L3-3B2':'HVAC'   , 'G2L3-3B3':'Laundry' , 'G2L3-3B4':'HVAC'   , 'G2L3-3B5':'Microwave_N' , 'G2L3-3B6':'MakeupAir_N' , 'G2L3-3B7':'Microwave_S' , 'G2L3-3B8':'MakeupAir_S' ,
                                 # 745
                                 'G3L1-1A1':'GenRec'  , 'G3L1-1A2':'Lights' , 'G3L1-1A3':'Kitchen' , 'G3L1-1A4':'Laundry' , 'G3L1-1A5':'GenRec' , 'G3L1-1A6':'Lights' , 'G3L1-1A7':'Kitchen' , 'G3L1-1A8':'Laundry' ,
                                 'G3L1-1B1':'GenRec'  , 'G3L1-1B2':'Lights' , 'G3L1-1B3':'Kitchen' , 'G3L1-1B4':'Laundry' , 'G3L1-1B5':'GenRec' , 'G3L1-1B6':'Lights' , 'G3L1-1B7':'Kitchen' , 'G3L1-1B8':'Laundry' ,
@@ -97,6 +97,8 @@ for file in listdir("."):
                                 'G3L3-3A1':'HVAC'    , 'G3L3-3A2':'Dryer'  , 'G3L3-3A3':'HVAC'    , 'G3L3-3A4':'Dryer'   , 'G3L3-3A5':'HVAC'   , 'G3L3-3A6':'Dryer'  , 'G3L3-3A7':'HVAC'    , 'G3L3-3A8':'Dryer'   ,
                                 'G3L3-3B1':'HVAC'    , 'G3L3-3B2':'Dryer'  , 'G3L3-3B3':'HVAC'    , 'G3L3-3B4':'Dryer'   , 'G3L3-3B5':'Water_Heater_N' , 'G3L3-3B6':'Microwave_N' , 'G3L3-3B7':'Water_Heater_S' , 'G3L3-3B8':'Microwave_S'
                             }
+
+                            # notes 2S has two spares
 
         with open(file, 'rU') as f:
             reader = csv.reader(f)
