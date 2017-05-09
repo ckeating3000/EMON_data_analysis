ccdf40044455ff5a11d23a6219a63c1026198a67
Colin Keating <ckeating3000@gmail.com>
2017-05-09 18:20:01 -0500

    more edits from meeting 5/9

--- get_month_summary.py
+++ get_month_summary.py
@@ -82,9 +82,72 @@ print subsystem_totals
 # print "total_749:" + str(total_749)
 
 # save output to CSV
+# with file(str(date) + '_report.csv', 'w') as outfile:
+#     # create column headers and write to outfile
+#     x=""
+#     for item in Subsystems:
+#         x+=item+","
+#     x=x[:-1]
+#     outfile.write(x)
+
+#     #todo: print row headers
+
+#     outfile.write('\n')
+
+    #slice_745=[][]
+    #slice_749=[][]
+
+    # for data_slice in subsystem_totals:
+    # #for index, data_slice in subsystem_totals:
+
+    #     #if index==0:
+    #     #    slice_745=data_slice
+    #         #print ""
+    #     #else:
+    #     #    slice_749=data_slice
+    #     #for line in data_slice:
+    #     #    print line
+    #         #x=np.array2string(line,separator=",")
+    #         #x+="test"
+    #         #print x
+    #         #np.savetxt(outfile, line, fmt='%-7.2f',delimiter=",")
+    #         #outfile.write('\n')
+    #     # The formatting string indicates that I'm writing out
+    #     # the values in left-justified columns 7 characters in width
+    #     # with 2 decimal places.
+
+    #     np.savetxt(outfile, data_slice, fmt='%-7.2f',delimiter=",")
+    #     # Writing out a break to indicate different slices...
+    #     outfile.write('\n')
+
+# output = np.asarray(subsystem_totals)
+# np.savetxt("_report.csv", output, delimiter=",")
+
+print "DONE"
+
+print "\n\n\n\n"
+
+print "test"
+
+print subsystem_totals[0,0,0]
+print subsystem_totals[0,0,1]
+print subsystem_totals[0,0,2]
+print Units[0]+","+str(subsystem_totals[0,0,0])+","+str(subsystem_totals[0,0,1])
+
+values_with_row_name = []
+
+for b_index, b_value in enumerate(Buildings):
+    for u_index, u_value in enumerate(Units):
+        x=""
+        for s_index, s_value in enumerate(Subsystems):
+            x+=","+str(subsystem_totals[b_index,u_index,s_index])
+        values_with_row_name.append(Buildings[b_index]+" "+Units[u_index]+x)
+        del x
+    values_with_row_name.append("")
+
 with file(str(date) + '_report.csv', 'w') as outfile:
     # create column headers and write to outfile
-    x=""
+    x=","
     for item in Subsystems:
         x+=item+","
     x=x[:-1]
@@ -94,34 +157,9 @@ with file(str(date) + '_report.csv', 'w') as outfile:
 
     outfile.write('\n')
 
-    slice_745=[][]
-    slice_749=[][]
+    for line in values_with_row_name:
+        outfile.write(line+"\n")
 
-    for index, data_slice in subsystem_totals:
-        if index==0:
-            slice_745=data_slice
-            #print ""
-        else:
-            slice_749=data_slice
-        for line in data_slice:
-            print line
-            #x=np.array2string(line,separator=",")
-            #x+="test"
-            #print x
-            #np.savetxt(outfile, line, fmt='%-7.2f',delimiter=",")
-            #outfile.write('\n')
-        # The formatting string indicates that I'm writing out
-        # the values in left-justified columns 7 characters in width
-        # with 2 decimal places.
-
-        np.savetxt(outfile, data_slice, fmt='%-7.2f',delimiter=",")
-        # Writing out a break to indicate different slices...
-        outfile.write('\n')
-
-# output = np.asarray(subsystem_totals)
-# np.savetxt("_report.csv", output, delimiter=",")
-
-print "DONE"
 
 
 
